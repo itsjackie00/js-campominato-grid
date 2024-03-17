7dd62c9a607b2d84334edca4677c5a78b2e32d18
itsjackie00 <jackiedanso33@gmail.com>
2024-03-17 21:46:36 +0100

    added ragionamento html

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto 
 // un messaggio in console con il numero della cella cliccata.
+
+// RAGIONAMENTO HTML
+/*
+# Creare una griglia per vedere le celle con i numeri da 1 a 100.
+# 100 / 10
+# <div id="boxes" class="d-flex justify-content-center align-content-center">
+    <div class="box"></div>
+</div> 
+*/
